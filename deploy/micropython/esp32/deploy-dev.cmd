51207ba84c14856b11c4fa342eed5d983f6fc6ff
Hans Christian Winther-Sorensen <hc@wsh.no>
2026-01-15 20:05:11 +0100

    Update make and deploy files

--- deploy/micropython/esp32/deploy-dev.cmd
+++ deploy/micropython/esp32/deploy-dev.cmd
@@ -1,2 +1,2 @@
 @echo off
-python -m esptool --chip esp32 --port %1 --baud 921600 --before default_reset --after hard_reset write_flash -z 0x1000 NODEMCU32S-v1.20.0-p0.2.4-2023-06-28.bin
+python -m esptool --chip esp32 --port %1 --baud 921600 --before default-reset --after hard-reset write-flash -z 0x1000 NODEMCU32S-v1.20.0-2026-01-11.bin
